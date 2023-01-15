46127175984380b0ba84767c06cd14f83a7f99d0
atias6051 <atias6051@gmail.com>
2023-01-14 21:42:19 -0500

    phase 4 completed

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -26,6 +26,18 @@ router.post(
     }
 );
 
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({ user: null });
+    }
+);
 
 router.delete(
     '/',
